27e0d58ad1e97b78a67b7264e4aa4bf6d015f576
Lucianocelli <lucia.nocelli@mi.unc.edu.ar>
2022-07-24 20:39:43 -0400

    Primer acercamiento al desafio de clase 6

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ function saludoDirecto() {
 //Definir tarea a realizar//
 let codigo = 0
 
-function cod () {
-    codigo = parseInt (prompt ('Ingrese el numero del area que te interesa: 1.Higiene personal  2.Belleza facial 3.Cuidado del cabello 0.Salir'));
+function cod() {
+    codigo = parseInt(prompt('Ingrese el numero del area que te interesa: 1.Higiene personal  2.Belleza facial 3.Cuidado del cabello 0.Salir'));
     switch (codigo) {
         case 1:
             clasificacionProductoA;
@@ -24,29 +24,29 @@ function cod () {
         case 2:
             let nuevoCodigo = 0
             do {
-                nuevoCodigo = prompt ('Ingrese la letra correspondiente: A.Exfoliante natural de arcilla verde B.Pulpa de carbon C.Agua de rosas');
-                if ((nuevoCodigo == "A")||(nuevoCodigo == "a")) {
+                nuevoCodigo = prompt('Ingrese la letra correspondiente: A.Exfoliante natural de arcilla verde B.Pulpa de carbon C.Agua de rosas');
+                if ((nuevoCodigo == "A") || (nuevoCodigo == "a")) {
                     clasificacionProductoA;
-                } else if ((nuevoCodigo == "B")||(nuevoCodigo == "b")) {
+                } else if ((nuevoCodigo == "B") || (nuevoCodigo == "b")) {
                     clasificacionProductoB;
-                } else if ((nuevoCodigo == "C")||(nuevoCodigo == "c")) {
+                } else if ((nuevoCodigo == "C") || (nuevoCodigo == "c")) {
                     clasificacionProductoC;
                 }
-            } while ((nuevoCodigo != "A")&&(nuevoCodigo != "a")&&(nuevoCodigo != "B")&&(nuevoCodigo != "b")&&(nuevoCodigo != "C")&&(nuevoCodigo != "c"))
+            } while ((nuevoCodigo != "A") && (nuevoCodigo != "a") && (nuevoCodigo != "B") && (nuevoCodigo != "b") && (nuevoCodigo != "C") && (nuevoCodigo != "c"))
             alert('En la consola te detallamos los productos disponibles de este filtro. Presione 0 para salir de las instrucciones por favor');
             break;
         case 3:
             let nuevoCodigoB = 0
             do {
-                nuevoCodigoB = prompt ('Ingrese la letra correspondiente: A.Shampoo solido B.Acondicionador solido C.Desenrredante natural');
-                if ((nuevoCodigoB == "A")||(nuevoCodigoB == "a")) {
+                nuevoCodigoB = prompt('Ingrese la letra correspondiente: A.Shampoo solido B.Acondicionador solido C.Desenrredante natural');
+                if ((nuevoCodigoB == "A") || (nuevoCodigoB == "a")) {
                     clasificacionProductoA;
-                } else if ((nuevoCodigoB == "B")||(nuevoCodigoB == "b")) {
+                } else if ((nuevoCodigoB == "B") || (nuevoCodigoB == "b")) {
                     clasificacionProductoB;
-                } else if ((nuevoCodigoB == "C")||(nuevoCodigoB == "c")) {
+                } else if ((nuevoCodigoB == "C") || (nuevoCodigoB == "c")) {
                     clasificacionProductoC;
                 }
-            } while ((nuevoCodigoB != "A")&&(nuevoCodigoB != "a")&&(nuevoCodigoB != "B")&&(nuevoCodigoB != "b")&&(nuevoCodigoB != "C")&&(nuevoCodigoB != "c"))
+            } while ((nuevoCodigoB != "A") && (nuevoCodigoB != "a") && (nuevoCodigoB != "B") && (nuevoCodigoB != "b") && (nuevoCodigoB != "C") && (nuevoCodigoB != "c"))
             alert('En la consola te detallamos los productos disponibles de este filtro. Presione 0 para salir de las instrucciones por favor');
             break;
         default:
@@ -54,73 +54,73 @@ function cod () {
     }
 }
 
-do { 
-    cod(); 
-} while (codigo !=0);
+do {
+    cod();
+} while (codigo != 0);
 
 //1.a Calcular costos totales de la compra realizada por el usuario//
-cod() 
+cod()
 
-class Higiene { 
-    constructor(marca,color,estilo) {
+class Higiene {
+    constructor(marca, color, estilo) {
         this.marca = marca,
-        this.color = color,
-        this.tipo = tipo,
-        this.precio = parseInt(Math.random()*10) //Math.random genera un numero entre 0 y 1
+            this.color = color,
+            this.tipo = tipo,
+            this.precio = parseInt(Math.random() * 10) //Math.random genera un numero entre 0 y 1
     }
-    mostrarProducto() { 
-        return ('Del producto '+this.tipo+' tenemos de la marca '+this.marca+' en color '+this.color+' y el precio es: '+this.precio);
+    mostrarProducto() {
+        return ('Del producto ' + this.tipo + ' tenemos de la marca ' + this.marca + ' en color ' + this.color + ' y el precio es: ' + this.precio);
     }
-    mostrarProducto() { 
-        return ('Del producto '+this.tipo+' tenemos de la marca '+this.marca+' en color '+this.color+' y el precio es: '+this.precio);
+    mostrarProducto() {
+        return ('Del producto ' + this.tipo + ' tenemos de la marca ' + this.marca + ' en color ' + this.color + ' y el precio es: ' + this.precio);
     }
 }
 
 class facial {
-    constructor(marca,color,tipo) {
+    constructor(marca, color, tipo) {
         this.marca = marca,
-        this.color = color,
-        this.tipo = tipo,
-        this.precio = parseInt(Math.random()*10) 
+            this.color = color,
+            this.tipo = tipo,
+            this.precio = parseInt(Math.random() * 10)
     }
-    mostrarProducto() { 
-        return ('Del producto '+this.tipo+' tenemos de la marca '+this.marca+' en color '+this.color+' y el precio es: '+this.precio);
+    mostrarProducto() {
+        return ('Del producto ' + this.tipo + ' tenemos de la marca ' + this.marca + ' en color ' + this.color + ' y el precio es: ' + this.precio);
     }
 }
 
 class cabello {
-    constructor(marca,color,tipo) {
+    constructor(marca, color, tipo) {
         this.marca = marca,
-        this.color = color,
-        this.tipo = tipo,
-        this.precio = parseInt(Math.random()*10) 
+            this.color = color,
+            this.tipo = tipo,
+            this.precio = parseInt(Math.random() * 10)
     }
-    mostrarProducto() { 
-        return ('Del producto '+this.tipo+' tenemos de la marca '+this.marca+' en color '+this.color+' y el precio es: '+this.precio);
+    mostrarProducto() {
+        return ('Del producto ' + this.tipo + ' tenemos de la marca ' + this.marca + ' en color ' + this.color + ' y el precio es: ' + this.precio);
     }
 }
 
-let Producto = [ 
-    new Produto ('Kamikaze','Blanco',Crema), 
-    new Produto ('Meraki','Azul',Cepillo),
-    new Produto ('Maggakup','Trasparente',Copita),
-    new Produto ('Kamikaze','Rosa',Arcilla),
-    new Produto ('Nodo','Blanco',Desodorante),
-    new Produto ('Ecopcion','Blanco',Bolsas),
-    new Produto ('Maggakup','Textura',Toallitas),
-    new Produto ('Kamikaze','Trasparente',Aceite),
-    new Produto ('Ecopcion','Gris',Sorbete),
-    new Produto ('Meraki','Marron',Estuche),
-    new Produto ('Nodo','Naranja',Bronceador),
-    new Produto ('Kalu','negro',Jalea),
-    new Produto ('Nodo','Verde',Pads)
+let Producto = [
+    new Produto('Kamikaze', 'Blanco', Crema),
+    new Produto('Meraki', 'Azul', Cepillo),
+    new Produto('Maggakup', 'Trasparente', Copita),
+    new Produto('Kamikaze', 'Rosa', Arcilla),
+    new Produto('Nodo', 'Blanco', Desodorante),
+    new Produto('Ecopcion', 'Blanco', Bolsas),
+    new Produto('Maggakup', 'Textura', Toallitas),
+    new Produto('Kamikaze', 'Trasparente', Aceite),
+    new Produto('Ecopcion', 'Gris', Sorbete),
+    new Produto('Meraki', 'Marron', Estuche),
+    new Produto('Nodo', 'Naranja', Bronceador),
+    new Produto('Kalu', 'negro', Jalea),
+    new Produto('Nodo', 'Verde', Pads)
 ];
 
 
-function clasificacionProducto(cod) { 
-    for (let cod of Higiene) { 
-        if (Higiene.Producto == cod) { 
-            Higiene.mostrarProducto(); 
+function clasificacionProducto(cod) {
+    for (let cod of Higiene) {
+        if (Higiene.Producto == cod) {
+            Higiene.mostrarProducto();
             console.log(Higiene)
         }
     }
@@ -138,14 +138,55 @@ for (let i = 1; i <= 2; i++) {
 
 alert("Nose quedamos sin stock por hoy")
 
-let nombreProductoA = "Crema"
+/*let nombreProductoA = "Crema"
 let precioProductoA = 1500
 let stockProductoA = 10
 
 let nombreProductoB = "Shampoo"
 let precioProductoB = 2000
-let stockProductoB = 50
+let stockProductoB = 50*/
 
+//Lo mismo que arriba pero todo junto//
+
+let productoA = {
+    nombre: "Crema",
+    precio: 1500,
+    stock: 10
+}
+
+let productoB = {
+    nombre: "Shampoo",
+    precio: 2000,
+    stock: 50
+}
+
+console.log(productoB.nombre)
+console.log(productoB.precio)
+console.log(productoB.stock)
+
+//Otra forma//
+const productoC = {
+    nombre: "Copita",
+    precio: 3500,
+    stock: 20
+}
+
+console.log(productoC["nombre"])
+console.log(productoC["precio"])
+console.log(productoC["stock"])
+
+//Otra forma//
+function Producto(nombre, precio, stock){
+    this.nombre = nombre
+    this.precio = precio
+    this.stock = stock
+}
+
+const productoD = new Producto("Pad", 500, 150)
+const productoE = new Producto("Sorbete", 350, 500)
+const productoF = new Producto("Bolsa", 1000, 50)
+
+//
 
 let cantidadCompras = prompt("Ingrese la cantidad de productos distintos que quiere comprar: \n- Shampoo\n- Crema")
 let precioTotal = 0;
@@ -160,24 +201,26 @@ for (let i = 0; i < cantidadCompras; i++) {
     let cantidad1 = prompt("Ingrese la cantidad de unidades que quiera comprar")
 
     if (compra1 == "Shampoo") {
-        if (stockProductoB >= cantidad1) {
-            precioTotal += cantidad1 * precioproductoB
-            alert("El precio total es de: $" + (cantidad1 * precioproductoB))
+        calculoStock(cantidad1, productoB.stock, productoB.precio)
+        if (productoB.stock >= cantidad1) {
+            precioTotal += cantidad1 * productoB.precio
+            alert("El precio total es de: $" + (cantidad1 * productoB.precio))
         } else {
-            alert("No disponemos de esa cantidad en stock actualmente. Nuestro stock actual es de: " + stockProductoB + "unidades")
+            alert("No disponemos de esa cantidad en stock actualmente. Nuestro stock actual es de: " + productoB.stock + "unidades")
         }
     } else if (compra1 == "Crema") {
-        if (stockProductoA >= cantidad1) {
-            precioTotal += cantidad1 * precioProductoA
-            alert("El precio total es de: $" + (cantidad1 * precioProductoA))
+        calculoStock(cantidad1, productoA.stock, productoA.precio)
+        if (productoA.stock >= cantidad1) {
+            precioTotal += cantidad1 * productoA.precio
+            alert("El precio total es de: $" + (cantidad1 * productoA.precio))
         } else {
-            alert("No disponemos de esa cantidad en stock actualmente. Nuestro stock actual es de: " + stockProductoA + "unidades")
+            alert("No disponemos de esa cantidad en stock actualmente. Nuestro stock actual es de: " + productoA.stock + "unidades")
         }
 
     } else {
         alert("No tenemos ese producto")
     }
-}
+} 
 
 if (cantidadCompras > 1) {
     alert("Este es el precio total de tu compra:" + precioTotal)
